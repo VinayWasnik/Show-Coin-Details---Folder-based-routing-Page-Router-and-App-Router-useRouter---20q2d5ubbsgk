3da54fb56fa9dbfd03746686bf3930a1721b6891
Vinay Wasnik <127746998+VinayWasnik@users.noreply.github.com>
2023-08-25 18:46:56 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,16 +5,25 @@ import CoinCard from './components/CoinCard';
 function Home() {
     const [coins, setCoins] = useState([]);
 
+    useEffect(() => {
+        async function fetchTopCoins() {
+            const response = await fetch('https://api.coinlore.net/api/tickers/');
+            const data = await response.json();
+            setCoins(data.data.slice(0,20));
+    }
+        fetchTopCoins();
+    }, []);
+
     return (
         <div className='home'>
             <h1>Top 20 Cryptos</h1>
             <div className='coins-container'>
                 {coins.map((coin) => (
-                    <CoinCard />
+                    <CoinCard key={coin.id} coin={coin} />
                 ))}
             </div>
         </div>
     )
 }
 
-export default Home
+export default Home;
