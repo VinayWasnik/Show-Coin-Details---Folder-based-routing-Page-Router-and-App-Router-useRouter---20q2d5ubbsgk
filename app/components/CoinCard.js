f03a0d860141e6499069c512a2ab36cb619669fc
Vinay Wasnik <127746998+VinayWasnik@users.noreply.github.com>
2023-08-25 19:58:40 +0530

    Update CoinCard.js

--- app/components/CoinCard.js
+++ app/components/CoinCard.js
@@ -4,11 +4,11 @@ import { Link } from "next/link";
 function CoinCard({ coin }) {
     return (
         <div className="coin-card">
-            <h3 className="coin-name">{coin.name}</h3>
-            <h4 className="coin-symbol">{coin.symbol}</h4>
-            <p className="coin-rank">Rank: {coin.rank}</p>
-            <p className="coin-price">Price: ${coin.price_usd}</p>
-            <Link href={`/${coin.id}`} className="view-details-link" href=""> 
+            <h3 className="coin-name">coin-name</h3>
+            <h4 className="coin-symbol">coin-symbol</h4>
+            <p className="coin-rank">Rank:coin-rank</p>
+            <p className="coin-price">Price:$coin-price_usd</p>
+            <Link className="view-details-link" href={`/${coin.id}`}> 
                 View Details
             </Link>
         </div>
